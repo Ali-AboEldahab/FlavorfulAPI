4eee4c59e3c334da243fd691a43b04c8b9bfee28
Ali-AboEldahab <3liaboeldahab@gmail.com>
2024-10-05 13:33:15 +0300

    Final

---
+++ Flavorful.Repository/Data/Config/OrderConfigConfiguration.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Flavorful.Repository.Data.Config
+{
+    internal class OrderConfigConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(o => o.ShippingAddress, S => S.WithOwner()); // 1:1 [Total Participation]
+
+            builder.Property(o => o.Status)
+                .HasConversion(
+
+                    OStatus => OStatus.ToString(),
+                    OStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus), OStatus)
+                );
+
+            builder.Property(o => o.Subtotal)
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasOne(o => o.DelivryMethod)
+                .WithMany()
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
